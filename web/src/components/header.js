74b8c64e8da1a1b8afeb2818af70e17592d8125e
hussamkhatib <hussamkhatib20@gmail.com>
2020-08-09 11:59:04 +0530

    added navigation bar(desktop and mobile)

--- web/src/components/header.js
+++ web/src/components/header.js
@@ -2,6 +2,12 @@ import {Link} from 'gatsby'
 import React from 'react'
 import Icon from './icon'
 import {cn} from '../lib/helpers'
+import logo from './icon/logo.svg'
+import asteroid from './icon/asteroid.svg'
+import astronaut from './icon/astronaut.svg'
+import vector from './icon/vector.svg'
+import team from './icon/team.svg'
+
 
 import styles from './header.module.css'
 
@@ -15,15 +21,41 @@ const Header = ({onHideNav, onShowNav, showNav, siteTitle}) => (
       <button className={styles.toggleNavButton} onClick={showNav ? onHideNav : onShowNav}>
         <Icon symbol='hamburger' />
       </button>
-
-      <nav className={cn(styles.nav, showNav && styles.showNav)}>
+  
+    
+     <nav className={cn(styles.nav, showNav && styles.showNav)}>
         <ul>
           <li>
-            <Link to='/archive/'>Archive</Link>
+            <Link to='/'>  
+            <img className={styles.logo} src={logo}></img>
+            Home
+            </Link>
           </li>
+          <li>
+            <Link to='/' className={styles.temp}>
+            <img src={vector}></img>
+              News</Link>
+          </li>
+          <li>
+          <Link to='/archive/'>
+          <img src={team}></img>  
+            Team</Link>
+          </li>
+          <li>
+          <Link to='/archive/'>
+          <img src={asteroid}></img>
+         Blog</Link>
+          </li>
+          <li>
+          <Link to='/archive/'>
+          <img src={astronaut}></img>
+            Contact</Link>
+          </li>
+
         </ul>
-      </nav>
-    </div>
+        </nav> 
+        
+    </div> 
   </div>
 )
 
